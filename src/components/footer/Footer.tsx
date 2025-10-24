d8b12dc1db7ca545d5b9e0cf5c942dd59418918b
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-10-24 09:35:00 -0400

    Atualização do rodapé

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { FacebookLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from "@phosphor-icons/react"
+import { GithubLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from "@phosphor-icons/react"
 
 function Footer(){
 
@@ -19,9 +19,17 @@ function Footer(){
           </p>
 
           <div className="flex gap-2">
-            <LinkedinLogoIcon size={48} weight="bold" />
-            <InstagramLogoIcon size={48} weight="bold" />
-            <FacebookLogoIcon size={48} weight="bold" />
+            <a href="https://www.linkedin.com/in/elzilanebarreto/" target="_blank">
+              <LinkedinLogoIcon size={48} weight="bold" />
+            </a>
+            
+            <a href="https://www.instagram.com/_elbarreto/" target="_blank">
+              <InstagramLogoIcon size={48} weight="bold" />
+            </a>
+            
+            <a href="https://github.com/elzilanebarreto" target="_blank">
+              <GithubLogoIcon size={48} weight="bold" />
+            </a>
           </div>
         </div>
       </div>
