0d0c134ea9bcfff86f1f649b0164c6dd79eb4495
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-10-24 09:30:28 -0400

    Atualização do navbar e colocando as rotas no app

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
   return(
     <>
@@ -6,7 +8,8 @@ function Navbar() {
                       bg-indigo-900 text-white">
 
         <div className="container flex justify-between text-lg mx-8">
-          Blog Pessoal
+          {/* Tag Link: navega internamente na aplicação */}
+          <Link to='/home' className="hover:underline text-2xl font-bold">Blog Pessoal</Link>
 
           <div className="flex gap-4">
             Postagens
