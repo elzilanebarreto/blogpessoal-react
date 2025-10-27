2811a3661d86ebe1aab9d348458a17b4efa75f48
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-10-27 12:27:45 -0400

    Criando o AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
-import Home from "./pages/home/Home"
+import { AuthProvider } from "./contexts/AuthContext";
 import Cadastro from "./pages/cadastro/Cadastro";
+import Home from "./pages/home/Home"
 import Login from "./pages/login/Login";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
 
-        <div className="min-h-[80vh]">
-          <Routes>
-            {/* Só a / será a tela inicial */}
-            <Route path="/" element={<Login />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/cadastro" element={<Cadastro />} />
-          </Routes>
-        </div>
+          <div className="min-h-[80vh]">
+            <Routes>
+              {/* Só a / será a tela inicial */}
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+            </Routes>
+          </div>
 
-        <Footer /> 
-      </BrowserRouter>
+          <Footer /> 
+        </BrowserRouter>
+      </AuthProvider>
     </>
   );
 }
