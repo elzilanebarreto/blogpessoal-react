c0be46c266e875f39c590f87ab61dc98ef6bba61
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-10-30 11:12:53 -0400

    Lista das postagens funcionando

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -26,7 +26,8 @@ function Navbar() {
           <Link to='/home' className="hover:underline text-2xl font-bold">Blog Pessoal</Link>
 
           <div className="flex gap-4">
-            Postagens
+            <Link to='/postagens' className="hover:underline">Postagens</Link>
+            
             <Link to='/temas' className="hover:underline">Temas</Link>
             
             <Link to='/cadastrartema' className="hover:underline">Cadastrar tema</Link>
--- src/components/postagem/cardpostagem/CardPostagem.tsx
+++ src/components/postagem/cardpostagem/CardPostagem.tsx
@@ -1,43 +1,57 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import type Postagem from "../../../models/Postagem";
 
-function CardPostagem() {
+interface CardPostagemProps {
+  postagem: Postagem;
+}
+function CardPostagem({ postagem }: CardPostagemProps) {
+
   return (
     <div className="border-slate-900 border flex flex-col rounded overflow-hidden justify-between">
-
       <div>
-
         <div className="flex w-full bg-indigo-400 py-2 px-4 items-center gap-4">
-          <img src="https://i.imgur.com/pK6vSCy.png" className="h-12 rounded-full" alt="" />
+          <img
+            src={postagem.usuario?.foto || "https://i.imgur.com/pK6vSCy.png"}
+            className="h-12 rounded-full"
+            alt=""
+          />
 
           <h3 className="text-lg font-bold text-center uppercase">
-            Nome do Usuário
+            {postagem.usuario?.nome || "Anônimo"}
           </h3>
         </div>
 
         <div className="p-4">
-          <h4 className="text-lg font-semibold uppercase">
-            Título
-          </h4>
+          <h4 className="text-lg font-semibold uppercase">{postagem.titulo}</h4>
 
-          <p>Texto</p>
-          <p>Tema: </p>
-          <p>Data: </p>
+          <p>{postagem.texto}</p>
+          <p>Tema: {postagem.tema?.descricao}</p>
+          <p>
+            Data:{new Intl.DateTimeFormat("pt-BR", {
+              dateStyle: 'full',
+              timeStyle: 'medium',
+            }).format(new Date(postagem.data))}
+          </p>
         </div>
-
       </div>
 
       <div className="flex">
-        <Link to='' className="w-full text-white bg-indigo-400 hover:bg-indigo-800 flex items-center justify-center py-2">
+        <Link
+          to=""
+          className="w-full text-white bg-indigo-400 hover:bg-indigo-800 flex items-center justify-center py-2"
+        >
           <button>Editar</button>
         </Link>
 
-        <Link to='' className="w-full text-white bg-red-400 hover:bg-red-700 flex items-center justify-center">
+        <Link
+          to=""
+          className="w-full text-white bg-red-400 hover:bg-red-700 flex items-center justify-center"
+        >
           <button>Deletar</button>
         </Link>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default CardPostagem
+export default CardPostagem;
